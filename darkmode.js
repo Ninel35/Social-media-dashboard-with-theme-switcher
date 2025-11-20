f8043b6b42662fac1160acbc398c92e78b0ecd70
Ninel35 <ninarasch3@gmail.com>
2025-11-20 23:43:30 +0100

    update toggle

--- darkmode.js
+++ darkmode.js
@@ -1,46 +1,23 @@
-let darkmode = localStorage.getItem('darkmode')
-const toggleswitch = document.getElementById('btn-toggle');
-
+let darkmode = localStorage.getItem("darkmode");
+const toggleswitch = document.getElementById("btn-toggle");
 
 const enableDarkmode = () => {
-    document.body.classList.add('darkmode')
-    localStorage.setItem('darkmode', 'active')
-}
+  document.body.classList.add("darkmode");
+  localStorage.setItem("darkmode", "active");
+  toggleswitch.checked = true;
+};
 
-const disableDarkmode = () =>{
-    document.body.classList.remove('darkmode')
-    localStorage.setItem('darkmode', null)
-}
+const disableDarkmode = () => {
+  document.body.classList.remove("darkmode");
+  localStorage.setItem("darkmode", null);
+  toggleswitch.checked = false;
+};
 if (localStorage.getItem("darkmode") === "active") {
-    enableDarkmode();
+  enableDarkmode();
 } else {
-    disableDarkmode();
+  disableDarkmode();
 }
 
-toggleswitch.addEventListener('change', ()=> {
- !toggleswitch.checked ? enableDarkmode() : disableDarkmode();
-})
-
-// toggleswitch.addEventListener('click', ()=> {
-//     darkmode = localStorage.getItem('darkmode')
-//     darkmode !== 'active' ? enableDarkmode() : disableDarkmode()
-// })
-
-
-// let darkmode = false;
-// const toggleswitch = document.getElementById('btn-toggle');
-
-// const enableDarkmode = () => {
-//     document.body.classList.add('darkmode')
-//     darkmode = true;
-// }
-
-// const disableDarkmode = () =>{
-//     document.body.classList.remove('darkmode')
-//     darkmode = false;
-// }
-
-// toggleswitch.addEventListener('click', ()=> {
-    
-//     !darkmode ? enableDarkmode() : disableDarkmode()
-// })
+toggleswitch.addEventListener("change", () => {
+  toggleswitch.checked ? enableDarkmode() : disableDarkmode();
+});
